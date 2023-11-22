052d2f73ee63aa4bb1fa828c2172861840456287
Alyosha333 <alexeiksefteris@protonmail.ch>
2023-11-22 15:28:17 +1100

    Finish createTroupe function

--- functions.js
+++ functions.js
@@ -63,19 +63,60 @@ function createMusician() {
 
 //End of createMusician function
 
-//Beginning of createTroupe function
+// Function to create a new troupe
 function createTroupe() {
+    // Displaying options to the user for selecting a troupe genre
     console.log("Create a New Troupe");
     console.log("Select the genre for the troupe");
     console.log("1. Rock");
     console.log("2. Jazz");
     console.log("3. Pop");
 
-    // Prompt the user to choose the genre
+    // Prompt the user to choose a genre from the options
     let genreChoice = prompt("Enter your choice (1 - 3): ");
 
+    // Validate the user's genre choice to ensure it's either 1, 2, or 3
+    while (genreChoice !== '1' && genreChoice !== '2' && genreChoice !== '3') {
+        console.log("Invalid choice. Please enter a number from 1 to 3.");
+        genreChoice = prompt("Enter your choice (1 - 3): ");
+    }
 
+    // Convert the numeric choice to a genre string
+    let genre = '';
+    switch (genreChoice) {
+        case '1':
+            genre = 'Rock';
+            break;
+        case '2':
+            genre = 'Jazz';
+            break;
+        case '3':
+            genre = 'Pop';
+            break;
+    }
+
+    // Prompt the user to enter the name of the troupe
+    let troupeName = prompt("Enter the troupe's name: ");
+    // Ensure that the troupe name is not empty
+    while (troupeName.length === 0) {
+        console.log("Troupe name cannot be empty.");
+        troupeName = prompt("Enter the troupe's name: ");
+    }
+
+    // Prompt the user to enter the troupe's performance duration
+    let duration = parseFloat(prompt("Enter troupe's performance duration (0.5 - 3 hours): "));
+    // Validate that the duration is a number and is within the allowed range
+    while (isNaN(duration) || duration < 0.5 || duration > 3){
+        console.log("Invalid duration. Please enter a value between 0.5 and 3 hours.");
+        duration = parseFloat(prompt("Enter troupe's performance duration (0.5 to 3 hours): "));
+    }
+
+    // Notify the user that the troupe has been created successfully
+    console.log(`Troupe '${troupeName}' in the genre of ${genre} with a duration of ${duration} hours created successfully.`);
+    // Create a new troupe object with the provided details and return it
+    return new Troupe(troupeName, genre, duration);
 }
+//End of addMusicianToTroupe function
 
 
 
@@ -98,4 +139,6 @@ function createTroupe() {
 
 
 
-module.exports = {createMusician}
+
+
+module.exports = {createMusician, createTroupe}
